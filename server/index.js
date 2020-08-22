f690374f2f54515244cb8abfebb051e9da08c758
Steven Walker <swalk001@gmail.com>
2020-08-21 21:56:25 -0400

    added docker compose for proxy deployment

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const path = require('path');
 const app = express();
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
-const port = '3000';
+const port = '4000';
 const photoTarget = 'http://localhost:3001';
 const sbTarget = 'http://localhost:3210';
 const reviewsTarget = 'http://localhost:2625';
@@ -11,13 +11,13 @@ const slnTarget = 'http://localhost:3005';
 
 app.use('/item/:id', express.static(path.join(__dirname, './../public')));
 
-app.use('*/photo/api', createProxyMiddleware({ target: photoTarget, changeOrigin: true }));
+app.use('*/photo/api', createProxyMiddleware({ target: 'http://3.23.171.227', changeOrigin: true }));
 
 app.use('*/sb/api', createProxyMiddleware({ target: sbTarget, changeOrigin: true }));
 
-app.use('*/reviews/api', createProxyMiddleware({ target: reviewsTarget, changeOrigin: true }));
+app.use('*/reviews/api', createProxyMiddleware({ target: 'http://50.18.135.245', changeOrigin: true }));
 
-app.use('*/sln/api', createProxyMiddleware({ target: slnTarget, changeOrigin: true }));
+app.use('*/sln/api', createProxyMiddleware({ target: 'http://50.18.3.201', changeOrigin: true }));
 
 
 app.listen(port, () => {
